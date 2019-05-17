ddf6d568882cb2b5d28e390ed804c85b2b440d9f
Mauricio Parra Casado <mauryparra@gmail.com>
2019-05-17 14:20:54 -0300

    Mostrar Trends en forma de tiles

--- app.js
+++ app.js
@@ -1,27 +1,143 @@
-// Bulma functions
-document.addEventListener('DOMContentLoaded', () => {
+var http = require('http');
+var https = require('https');
 
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+var url = require('url');
+var fs = require('fs');
 
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
+const url_trends = 'https://api.mercadolibre.com/trends/{0}';
+const url_trends_cat = 'https://api.mercadolibre.com/trends/{0}/{1}';
 
-        // Add a click event on each of them
-        $navbarBurgers.forEach( el => {
-            el.addEventListener('click', () => {
+http.createServer( function (req, res) {
+    var q = url.parse(req.url, true);
 
-                // Get the target from the "data-target" attribute
-                const target = el.dataset.target;
-                const $target = document.getElementById(target);
+    switch (q.pathname) {
+        case '/':
+            loadIndex(req, res);
+            break;
+        case '/trends':
+            loadTrends(req, res);
+            break;
+        case '/api/trends':
+            apiGetTrends(req, res);
+            break;
+        default:
+            loadFile(req, res);
+            break;
+    }
+}).listen(8080);
 
-                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-                el.classList.toggle('is-active');
-                $target.classList.toggle('is-active');
+function loadIndex(req, res) {
+    fs.readFile('./index.html', function (err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        }
 
-            });
-        });
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    })
+}
+
+function loadTrends(req, res) {
+    fs.readFile('./trends.html', function (err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        }
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    })
+}
+
+function apiGetTrends(req, res) {
+
+    var q = url.parse(req.url, true);
+    var api_url = '';
+
+    var trends = '';
+    var html = '';
+
+    if (q.query.category_id) {
+        api_url = 'https://api.mercadolibre.com/trends/' + q.query.site_id + '/' + q.query.category_id;
+    } else {
+        api_url = 'https://api.mercadolibre.com/trends/' + q.query.site_id;
     }
 
-});
+    https.get(api_url, function(resp){
+        var data = '';
 
+        resp.on('data', function(chunk){
+            data += chunk;
+        });
+
+        resp.on('end', function(){
+            trends = JSON.parse(data);
+            html = obtenerHTMLTrends(trends, q.query.fil, q.query.col, q.query.visualizacion)
+
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(html);
+            res.end();
+
+        });
+    }).on('error', function(e){
+        console.log("Ocurrio un error", e);
+    });
+}
+
+function obtenerHTMLTrends(trends, fil, col, visualizacion) {
+    var html = '';
+    var trendIndex = 0;
+    var colores = ['is-primary', 'is-success', 'is-info', 'is-warning', 'is-danger', 'is-dark', 'is-light'];
+
+    for (let i = 0; i < fil; i++) {
+        html += '<div class="tile is-ancestor">';
+
+        for (let j = 0; j < col; j++) {
+            if (trendIndex < trends.length) {
+
+                if (visualizacion === 'imagen') {
+                    html += '<div class="tile is-parent">' +
+                    '<article class="tile is-child box has-text-centered notification '+ colores[Math.floor(Math.random()*colores.length)] + '">' +
+                    '<p class="title">' + trends[trendIndex].keyword + '</p>' +
+                    '<figure class="image is-4by3"><img src="https://bulma.io/images/placeholders/640x480.png"></figure>' +
+                    '<a href="' + trends[trendIndex].url + '" class="subtitle " target="_blank">Ver Items</a>' +
+                    '</article>' +
+                    '</div>';
+                } else if (visualizacion === 'texto') {
+                    html += '<div class="tile is-parent">' +
+                    '<article class="tile is-child box has-text-centered notification '+ colores[Math.floor(Math.random()*colores.length)] + '">' +
+                    '<p class="title">' + trends[trendIndex].keyword + '</p>' +
+                    '<a href="' + trends[trendIndex].url + '" class="subtitle " target="_blank">Ver Items</a>' +
+                    '</article>' +
+                    '</div>';
+                }
+
+
+            }
+            trendIndex++;
+        }
+
+        html += '</div>';
+    }
+
+    html += '</div>';
+    return html;
+}
+
+function loadFile(req, res) {
+    var q = url.parse(req.url, true);
+
+    fs.readFile('.' + q.pathname, function (err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        }
+
+        res.writeHead(200);
+        res.write(data);
+        return res.end();
+    })
+}
---
+++ trends.js
@@ -0,0 +1,46 @@
+function getTrends() {
+
+    // Create a request variable and assign a new XMLHttpRequest object to it.
+    var request = new XMLHttpRequest();
+
+    // Open a new connection, using the GET request on the URL endpoint
+    request.open('GET', '/api/trends?' + window.location.search.slice(1), true);
+
+    request.onload = function () {
+        // Begin accessing JSON data here
+        document.getElementById('trends').innerHTML = request.responseText;
+    };
+
+    // Send request
+    request.send();
+}
+
+
+
+
+// Bulma functions
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+
+        // Add a click event on each of them
+        $navbarBurgers.forEach( el => {
+            el.addEventListener('click', () => {
+
+                // Get the target from the "data-target" attribute
+                const target = el.dataset.target;
+                const $target = document.getElementById(target);
+
+                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+
+            });
+        });
+    }
+
+});
